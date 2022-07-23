20f08fafb6a4ea62c83c393c506de61d8482f804
Ashish33000 <87129673+Ashish33000@users.noreply.github.com>
2022-07-24 01:40:09 +0530

    Add files via upload

---
+++ Everhour/script/para10.js
@@ -0,0 +1,26 @@
+function paraTen(){
+  return `<div id="para10">
+  <div>
+    <img src="https://blog-cdn.everhour.com/assets/images/new-design/illustrations/resources/section-guide-and-templates.webp" alt="">
+    <p>Resources</p>
+    <a href="https://everhour.com/resources">Free guides and templates to help your team work better</a>
+  </div>
+  <div>
+    <img src="https://blog-cdn.everhour.com/assets/images/new-design/illustrations/resources/section-tutorial-videos.webp" alt="">
+    <p>Video tutors</p>
+    <a href="https://www.youtube.com/playlist?list=PLpi40TLI2leiI-UuWIu-19xdTuGZwtHU3">Short video to learn the best way to use everhour</a>
+
+  </div>
+  <div>
+    <img src="https://blog-cdn.everhour.com/assets/images/new-design/illustrations/resources/section-product-updates.webp" alt="">
+    <p>Blog</p>
+    <a href="https://everhour.com/blog/category/product-updates/">Your first destination for the freshly release everhour features</a>
+  </div>
+  <div>
+    <img src="https://blog-cdn.everhour.com/assets/images/new-design/illustrations/resources/section-help-docs.webp" alt="">
+    <p>Help center</p>
+    <a href="">FAQ, tutorials,documantation help and support</a>
+  </div>
+</div>`
+}
+export default paraTen()
---
+++ Everhour/script/para2.js
@@ -0,0 +1,8 @@
+
+function videoPara(){
+  return `<div id="para2">
+  <a href="https://youtu.be/k2OeJBqXccI?t=6"><img src="https://blog-cdn.everhour.com/assets/images/new-design/illustrations/primary-illustrations/home-narrow-v2.webp" alt=""></a>
+</div>`
+}
+
+export default videoPara()
